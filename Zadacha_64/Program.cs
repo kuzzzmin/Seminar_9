32cd0e23dcdfefc7c0dae6ce880c255d36144f3d
unknown <kuzy2508@mail.ru>
2022-11-25 23:02:04 +0300

    Zadacha_64

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 64: Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+int NumbR(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    Console.WriteLine(n);
+    return NumbR(n - 1);
+}
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int f = NumbR(n);
+Console.WriteLine(f);
